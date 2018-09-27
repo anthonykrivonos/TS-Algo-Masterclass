ac456715591ee5de080ac7f33e0dfb0fb109da31
anthonykrivonos <abkrivonos@gmail.com>
2018-09-26 22:01:23 -0400

    #0.0.6b - Added Maps

--- src/maps/main/maps-main.ts
+++ src/maps/main/maps-main.ts
@@ -10,8 +10,35 @@ import * as Utility from "../../utility";
 /**
  * Masterclass hash map/hash table.
  */
-export class MCMap<T> {
+export class MCMap<T> extends Map<T, T> {
 
-      
+      /**
+       * Constructor
+       * - Creates an MCMap from a regular map.
+       * @param map Optional map to reconstruct.
+       */
+      constructor(map:Map<T, T> | null = null) {
+            map ? super(map) : super();
+            Object.setPrototypeOf(this, Object.create(Map.prototype));
+      }
+
+      /**
+       * Copy
+       * - Copies the MCMap and returns its replica.
+       * @returns The new MCMap.
+       */
+      public copy():MCMap<T> {
+            let map:MCMap<any> = new MCMap(this);
+            return map;
+      }
+
+      /**
+       * Get Sorted
+       * - Sorts the MCMap by key, ascending.
+       * @returns The sorted MCMap.
+       */
+      public getSorted():MCMap<T> {
+            return new MCMap<T>(new Map([...this.entries()].sort()));
+      }
 
 }
