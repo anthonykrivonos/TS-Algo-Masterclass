5932b2c9b789ae7c33783752a6ed377b72abcf1c
Anthony Krivonos <abkrivonos@gmail.com>
2018-09-29 23:20:48 -0400

    #0.0.7b - BFS Implementation

--- src/graphs/main/graphs-main.ts
+++ src/graphs/main/graphs-main.ts
@@ -1,36 +1,36 @@
 /**
- * Anthony Krivonos
- * Typescript Algorithm Masterclass
- * 09.27.2018
- * Graphs/Main
- */
+* Anthony Krivonos
+* Typescript Algorithm Masterclass
+* 09.27.2018
+* Graphs/Main
+*/
 
-import * as Utility from "../../utility";
-import { MCArray } from "../../arrays/main/arrays-main"
-import { MCMap } from "../../maps/main/maps-main"
+import { MCArray } from "../../arrays/main/arrays-main";
+import { MCMap } from "../../maps/main/maps-main";
+import { MCQueue } from "../../queues/main/queues-main";
 
 /**
- * Masterclass graph implementation.
- */
+* Masterclass graph implementation.
+*/
 export class MCGraph<T> {
 
       private adjacencyMap:MCMap<T, MCArray<T>>;
 
       /**
-       * Constructor
-       * - Creates an MCMap from a regular map.
-       * @param map Optional map to reconstruct.
-       */
+      * Constructor
+      * - Creates an MCMap from a regular map.
+      * @param map Optional map to reconstruct.
+      */
       constructor() {
             this.adjacencyMap = new MCMap<T, MCArray<T>>();
       }
 
       /**
-       * Add Vertex
-       * - Adds a vertex to the graph.
-       * @param vertex Vertex to add.
-       * @returns True if the vertex was added, false otherwise.
-       */
+      * Add Vertex
+      * - Adds a vertex to the graph.
+      * @param vertex Vertex to add.
+      * @returns True if the vertex was added, false otherwise.
+      */
       public addVertex(vertex:T):boolean {
             if (!this.adjacencyMap.has(vertex)) {
                   this.adjacencyMap.set(vertex, new MCArray<T>());
@@ -40,12 +40,12 @@ export class MCGraph<T> {
       }
 
       /**
-       * Add Edge
-       * - Adds a vertex to the graph.
-       * @param from One vertex to create the edge from.
-       * @param to One vertex to create the edge to.
-       * @returns True if the edge was added, false otherwise.
-       */
+      * Add Edge
+      * - Adds a vertex to the graph.
+      * @param from One vertex to create the edge from.
+      * @param to One vertex to create the edge to.
+      * @returns True if the edge was added, false otherwise.
+      */
       public addEdge(from:T, to:T):boolean {
             if (this.adjacencyMap.has(from) && !this.adjacencyMap.get(from)!.includes(to) && this.adjacencyMap.has(to) && !this.adjacencyMap.get(to)!.includes(from)) {
                   this.adjacencyMap.get(from)!.push(to);
@@ -56,12 +56,12 @@ export class MCGraph<T> {
       }
 
       /**
-       * Remove Vertex
-       * - Removes a vertex from the graph.
-       * - Removes all edges connected to other vertices.
-       * @param vertex Vertex to add.
-       * @returns True if the vertex was removed, false otherwise.
-       */
+      * Remove Vertex
+      * - Removes a vertex from the graph.
+      * - Removes all edges connected to other vertices.
+      * @param vertex Vertex to add.
+      * @returns True if the vertex was removed, false otherwise.
+      */
       public removeVertex(vertex:T):boolean {
             if (this.adjacencyMap.has(vertex)) {
                   this.adjacencyMap.delete(vertex);
@@ -78,12 +78,12 @@ export class MCGraph<T> {
       }
 
       /**
-       * Remove Edge
-       * - Removes an edge from the graph.
-       * @param from One vertex to remove the edge from.
-       * @param to One vertex to remove the edge to.
-       * @returns True if the edge was removed, false otherwise.
-       */
+      * Remove Edge
+      * - Removes an edge from the graph.
+      * @param from One vertex to remove the edge from.
+      * @param to One vertex to remove the edge to.
+      * @returns True if the edge was removed, false otherwise.
+      */
       public removeEdge(from:T, to:T):boolean {
             if (this.adjacencyMap.has(from) && this.adjacencyMap.get(from)!.includes(to) && this.adjacencyMap.has(to) && this.adjacencyMap.get(to)!.includes(from)) {
                   let indexOfTo = this.adjacencyMap.get(from)!.indexOf(to);
@@ -92,6 +92,42 @@ export class MCGraph<T> {
                   this.adjacencyMap.get(to)!.splice(indexOfFrom, 1);
                   return true;
             }
+            return false;
+      }
+
+      /**
+      * Breadth First Traversal
+      * - O(n)
+      * - Search each neighboring vertex.
+      * - If not found, search the depth of each neighboring vertex.
+      * - Keep track of checked vertices.
+      * @param from Vertex to start the search from.
+      * @param for Vertex to search for.
+      * @returns True if the vertex was found, false otherwise.
+      */
+      public breadthFirstSearch(from:T, find:T):boolean {
+            var visited = new MCArray();
+            var visit = new MCQueue();
+
+            // Visit the from element
+            visited.push(from);
+
+            // Recur while visit queue is non-empty
+            while (!visit.isEmpty()) {
+                  let currentVertex = visit.poll() as T;
+
+                  if (currentVertex === find) {
+                        return true;
+                  }
+
+                  this.adjacencyMap.get(currentVertex)!.forEach((vertex) => {
+                        if (!visited.includes(vertex)) {
+                              visited.push(vertex);
+                              visit.add(vertex);
+                        }
+                  });
+            }
+
             return false;
       }
 
